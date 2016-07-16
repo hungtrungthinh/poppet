ba48f4bb33d363b639134788558528f93e01a8cd
Rakesh Mukundan <rakesh@rakeshmukundan.in>
2016-07-16 19:35:48 +0200

    Updated install script to set unifi port by default

--- scripts/install.sh
+++ scripts/install.sh
@@ -54,6 +54,7 @@ fi
 .env/bin/python manage.py init_data
 .env/bin/python manage.py rebuild_daily_stat
 .env/bin/python manage.py migrate_vouchers
+.env/bin/python manage.py set_unifiport
 
 if [  -d /etc/init/uwsgi.conf ]; then
     echo '-------------Upstart UWSGI config found, removing and upgrading to supervisor--------------'
